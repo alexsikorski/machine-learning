a5c4e3a35bbc89e4332c46980ae39fe755054433
alexsikorski <alexsikorski22@gmail.com>
2020-08-20 17:50:22 +0100

    coefficients and intercept are estimated and an accuracy is returned

--- linear_reg.py
+++ linear_reg.py
@@ -1,13 +1,32 @@
-import sklearn
 import matplotlib.pyplot as plt
 import numpy as np
 import random
+from sklearn import model_selection
+from sklearn import linear_model
 
-x = list(range(0, 50))  # celcius
+# populating data
+x = list(range(0, 20))  # celcius
 y = [1.8 * F + 32 + random.randint(-3, 3) for F in x]  # farenheit
+# y = [1.8 * F + 32 for F in x]  # farenheit
 
 print(f'x: {x}')
 print(f'y: {y}')
 
 plt.plot(x, y, '-*r')
 plt.show()
+
+# required format for machine learning library
+x = np.array(x).reshape(-1, 1)
+y = np.array(y).reshape(-1, 1)
+
+x_training, x_testing, y_training, y_testing = model_selection.train_test_split(x, y, test_size=0.2)
+# test_size 20%, training is therefore 80%
+
+model = linear_model.LinearRegression()
+model.fit(x_training, y_training)
+
+print(f'Coefficients: {model.coef_}')  # this is m in y = mx + c, should be 1.8
+print(f'Intercept: : {model.intercept_}')  # this is the c in y = mx + c, should be 32
+
+accuracy = model.score(x_testing, y_testing)
+print(f'Accuracy: {round(accuracy * 100, 2)}')
