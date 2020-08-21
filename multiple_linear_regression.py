024a68aaaa4f7ab69f24e6cb005f7a9a47c6250a
alexsikorski <alexsikorski22@gmail.com>
2020-08-21 15:14:24 +0100

    checked for NaN values and replaced with 0

--- multiple_linear_regression.py
+++ multiple_linear_regression.py
@@ -10,7 +10,7 @@ from sklearn import metrics
 pd.set_option('display.max_columns', None)
 
 # loading data
-data = pd.read_csv('top-youtube-videos.csv', sep=',')
+data = pd.read_csv('top-youtube-videos.csv')
 data = data[['channelId', 'comments', 'duration', 'definition', 'licensedContent', 'likes', 'dislikes', 'views']]
 
 # processing data
@@ -18,23 +18,36 @@ le = preprocessing.LabelEncoder()
 data['definition'] = le.fit_transform(data['definition'])  # 0 is HD 1 is SD
 data['licensedContent'] = le.fit_transform(data['licensedContent'])  # 1 is TRUE 0 is FALSE
 
-x = data.iloc[:, :-1]  # without views
+X = data.iloc[:, :-1]  # without views
 y = data.iloc[:, 7]  # views isolated
 
 # converting unique channels to categorical columns
-channel_ids = pd.get_dummies(x['channelId'], drop_first=True)  # dummy variable trap dealt with
+channel_ids = pd.get_dummies(X['channelId'], drop_first=True)  # dummy variable trap dealt with
 
 # dropping chanelId column as dummies are calculated
-x = x.drop('channelId', axis=1)
+X = X.drop('channelId', axis=1)
 
 # concat dummy variables
-x = pd.concat([x, channel_ids], axis=1)
+X = pd.concat([X, channel_ids], axis=1)
+
+# convert everything to a float
+X = X.astype(float)
+y = y.astype(float)
+
+# check for NaN and inf
+# print("NaN X:", np.any(np.isnan(X)),"NaN y:", np.any(np.isnan(y)), "|", "Inf X:", np.all(np.isfinite(X)),  "Inf y:", np.all(np.isfinite(y)))
+X = X.fillna(0)
 
 # training and testing
-x_train, x_test, y_train, y_test = model_selection.train_test_split(x, y, test_size=0.2, random_state=0)
+X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.2, random_state=0)
 
 # fitting multiple linear regression to training set
 reg = linear_model.LinearRegression()
-reg.fit(x_train, y_train)
+
+reg.fit(X_train, y_train)
+
+# predict the test set results
+y_pred = reg.predict(X_test)
+
 
 
