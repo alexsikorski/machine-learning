1f9f10eac75a6f1464a8148c1bf69b067056fe55
alexsikorski <alexsikorski22@gmail.com>
2020-08-25 14:13:43 +0100

    fixed reshaping, added model scoring

--- coronavirus-predictor/coronavirus_predictor.py
+++ coronavirus-predictor/coronavirus_predictor.py
@@ -2,12 +2,14 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import PolynomialFeatures
+from sklearn import linear_model
 
 fields = ['World']  # only obtaining world column
 df = pd.read_csv('total_cases.csv', skipinitialspace=True, usecols=fields)
+df['id'] = df.index  # copy index to id column
 
-X = np.array(df.index)
-y = np.array(df['World'])
+X = np.array(df['id']).reshape(-1, 1)
+y = np.array(df['World']).reshape(-1, 1)
 
 plt.plot(y, '-b')  # actual data is blue
 plt.show()
@@ -15,3 +17,8 @@ plt.show()
 poly_features = PolynomialFeatures(degree=2)
 X = poly_features.fit_transform(X)
 
+print('--- training ---')
+model = linear_model.LinearRegression()
+model.fit(X, y)
+acc = model.score(X, y)
+print(f'Accuracy: {round(acc*100, 3)}%')
