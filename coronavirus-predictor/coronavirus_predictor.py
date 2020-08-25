13c1f240b11a405946021b0a68f7a76ac366eebe
alexsikorski <alexsikorski22@gmail.com>
2020-08-25 14:38:47 +0100

    prediction is now available and displayed in dashed yellow line

--- coronavirus-predictor/coronavirus_predictor.py
+++ coronavirus-predictor/coronavirus_predictor.py
@@ -11,11 +11,7 @@ df['id'] = df.index  # copy index to id column
 X = np.array(df['id']).reshape(-1, 1)
 y = np.array(df['World']).reshape(-1, 1)
 
-plt.plot(y, '-b')  # actual data is blue
-plt.xlabel('days')
-plt.ylabel('coronavirus cases')
-
-poly_features = PolynomialFeatures(degree=2)
+poly_features = PolynomialFeatures(degree=4)
 X = poly_features.fit_transform(X)
 
 print('--- training ---')
@@ -24,6 +20,23 @@ model.fit(X, y)
 acc = model.score(X, y)
 print(f'Accuracy: {round(acc*100, 3)}%')
 
+# testing
 y0 = model.predict(X)
-plt.plot(y0, '--r')  # predicted data is dashed red
+
+print('--- prediction ---')
+days = 30
+print(f'prediction - cases after {days} days: ', end='')
+print(round(int(model.predict(poly_features.fit_transform([[240 + days]]))) / 1000000, 2), 'million')
+# the data has gathered 241 days worth of data (index starts at 0 so 240 is used)
+# changing days variable will display appropriate prediction.
+
+x1 = np.array(list(range(1, 240 + days))).reshape(-1, 1)
+y1 = model.predict(poly_features.fit_transform(x1))
+
+plt.xlabel('days')
+plt.ylabel('coronavirus cases')
+
+plt.plot(y, '-b')  # actual data is blue
+plt.plot(y1, '--y')  # prediction of the future
+plt.plot(y0, '--r')  # line of best fit
 plt.show()
