c085532b8edf80997c1e32ca54c21ab9ff96f282
alexsikorski <alexsikorski22@gmail.com>
2020-08-25 13:58:23 +0100

    pre-processing

--- coronavirus-predictor/coronavirus_predictor.py
+++ coronavirus-predictor/coronavirus_predictor.py
@@ -1,6 +1,17 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import PolynomialFeatures
 
 fields = ['World']  # only obtaining world column
 df = pd.read_csv('total_cases.csv', skipinitialspace=True, usecols=fields)
-print(df.head())
+
+X = np.array(df.index)
+y = np.array(df['World'])
+
+plt.plot(y, '-b')  # actual data is blue
+plt.show()
+
+poly_features = PolynomialFeatures(degree=2)
+X = poly_features.fit_transform(X)
 
