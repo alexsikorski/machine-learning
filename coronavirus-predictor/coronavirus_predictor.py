3f60fae0249bb618fae679e8dea52cda8ed07ba6
alexsikorski <alexsikorski22@gmail.com>
2020-08-25 14:16:23 +0100

    line of best fit for one curve is displayed

--- coronavirus-predictor/coronavirus_predictor.py
+++ coronavirus-predictor/coronavirus_predictor.py
@@ -12,7 +12,8 @@ X = np.array(df['id']).reshape(-1, 1)
 y = np.array(df['World']).reshape(-1, 1)
 
 plt.plot(y, '-b')  # actual data is blue
-plt.show()
+plt.xlabel('days')
+plt.ylabel('coronavirus cases')
 
 poly_features = PolynomialFeatures(degree=2)
 X = poly_features.fit_transform(X)
@@ -22,3 +23,7 @@ model = linear_model.LinearRegression()
 model.fit(X, y)
 acc = model.score(X, y)
 print(f'Accuracy: {round(acc*100, 3)}%')
+
+y0 = model.predict(X)
+plt.plot(y0, '--r')  # predicted data is dashed red
+plt.show()
